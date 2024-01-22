4f29c47c3ad7eacf658b97e08d5df93798840b73
ShanzidaRahmanMim <shanzidamim33@gmail.com>
2024-01-22 15:38:42 +0600

    modified java file

--- first.java
+++ first.java
@@ -8,3 +8,14 @@ class Singleton{
         return ins;
     }
 }
+class GFG{
+    public static void main(String args[])
+    {
+        Singleton x=Singleton.getInstance();
+        Singleton y=Singleton.getInstance();
+        Singleton z=Singleton.getInstance();
+        System.out.println("Hashcode of x is" +x.hashCode());
+        System.out.println("Hashcode of y is" +y.hashCode());
+        System.out.println("Hashcode of z is" +z.hashCode());
+    }
+}
